7525f2dcf915029f183d784cc0387bed5cd82801
daithilacha <david.oconnor4@students.ittralee.ie>
2017-11-09 11:45:54 +0000

    Added abstract Person Class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,27 @@
-public abstract class Person {
+public abstract class Person implements PlayerActions {
+    private String name;
+
+
+    public Person() {
+        this("Unknown");
+    }
+
+    public Person(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void rollDice();
+
+    public String toString() {
+        return "Name: " + getName();
+    }
 }
