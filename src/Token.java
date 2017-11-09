6e57e7949dffbc57a6180b52e0f875a861b95c85
daithilacha <david.oconnor4@students.ittralee.ie>
2017-11-09 11:48:59 +0000

    Added Token Class

---
+++ src/Token.java
@@ -0,0 +1,24 @@
+public class Token {
+    private String colour;
+
+    public Token() {
+        this("Unspecified");
+    }
+
+    public Token(String colour) {
+        setColour(colour);
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Token colour: " + getColour();
+    }
+}
